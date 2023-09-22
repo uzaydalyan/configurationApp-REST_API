8f03d99af4abe7f5b92a0f6bdb21dc58a3c8ee9f
uzaydalyan <uzaydalyan@gmail.com>
2023-09-22 22:06:01 +0300

    fix in private key

--- app.ts
+++ app.ts
@@ -26,7 +26,9 @@ function initFirebase() {
             "type": "service_account",
             "project_id": "configurationapp-c218c",
             "private_key_id": process.env.PRIVATE_KEY_ID,
-            "private_key": process.env.PRIVATE_KEY,
+            "private_key": process.env.PRIVATE_KEY
+                ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
+                : undefined,
             "client_email": process.env.CLIENT_EMAIL,
             "client_id": process.env.CLIENT_ID,
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
