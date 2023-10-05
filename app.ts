ceabcd110a86084cdfae5dde03cad08211c77ba0
uzaydalyan <79166089+uzaydalyan@users.noreply.github.com>
2023-10-05 15:15:55 +0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -23,16 +23,16 @@ function initFirebase() {
     admin.initializeApp({
         credential: admin.credential.cert({
             "type": "service_account",
-            "project_id": "configurationapp-c218c",
+            "project_id": process.env.PROJECT_ID,
             "private_key_id": process.env.PRIVATE_KEY_ID,
             "private_key": process.env.PRIVATE_KEY
                 ? process.env.PRIVATE_KEY.replace(/\\n/g, "\n")
                 : undefined,
             "client_email": process.env.CLIENT_EMAIL,
             "client_id": process.env.CLIENT_ID,
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+            "auth_uri": process.env.AUTH_URI",
+            "token_uri": process.env.TOKEN_URI,
+            "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT_URL,
             "client_x509_cert_url": process.env.CLIENT_CERT_URL,
             "universe_domain": "googleapis.com"
         })
